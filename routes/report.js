0a4b36a0a48d0ed7254c8cc64ff77d7ddee50edd
meri2233 <85399333+Meri2233@users.noreply.github.com>
2022-09-20 08:33:21 +0545

    Update report.js

--- routes/report.js
+++ routes/report.js
@@ -3,22 +3,25 @@ const router = express.Router();
 const reportModel = require('../models/report.model');
 
 router.post('/add', async (req, res) => {
-    const { title, students, scores } = req.body;
+    const { title, students, scores, winner } = req.body;
+
     const newReport = new reportModel({
+        title: title,
         students: students,
         scores: scores,
+        winner: winner,
         createdBy: req.userInfo.id
     })
     try {
         const savedReport = await newReport.save();
-        res.status(200).send("Added new Teacher with id: " + savedReport._id);
+        res.status(200).send("Added new Report with id: " + savedReport._id);
     }
     catch (e) {
         res.status(501).send(e.message);
     }
 })
 
-router.get('/list', async(req, res) => {
+router.get('/list', async (req, res) => {
     const myReports = await reportModel.find({ createdBy: req.userInfo.id });
     res.status(200).send(myReports);
 })
